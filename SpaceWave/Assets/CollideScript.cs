ced4b9f20452b1c79e4670676080919b62e81d3f
kkunan <kkunan@essex.ac.uk>
2017-01-20 19:44:01 +0000

    Initial commit

---
+++ SpaceWave/Assets/CollideScript.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class CollideScript : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        Debug.Log("OUCH!");
+    }
+}
---
+++ SpaceWave/Assets/ExampleScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	//	Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
+	//	rb.AddForce(new Vector2(0f, 10f), ForceMode2D.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
