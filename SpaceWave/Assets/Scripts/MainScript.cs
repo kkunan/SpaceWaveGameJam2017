7f7a0a5175cc661fb81d0c508453809c76f1f2be
kkunan <kkunan@essex.ac.uk>
2017-01-20 21:27:28 +0000

    added turning, asteroid spawning, basic ui

---
+++ SpaceWave/Assets/Scripts/MainScript.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainScript : MonoBehaviour
+{
+
+	public GameObject station;
+	public Text turnText;
+	public Text scoreValueText;
+	public Text livesValueText;
+	public Text resourcesValueText;
+
+	public float score;
+	public float lives;
+	public float resources;
+
+	public GameObject asteroid;
+	public float asteroidSpawnMinTime = 20; // seconds
+	public float asteroidSpawnMaxTime = 50;
+	private float asteroidSpawnCounter = 0;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		// stationScript = station.GetComponent<SpaceStationScript>();
+		asteroidSpawnCounter = UnityEngine.Random.Range(asteroidSpawnMinTime, asteroidSpawnMaxTime);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		turnText.text = String.Format("Turn {0:F} degs", station.transform.rotation.eulerAngles.z);
+
+		asteroidSpawnCounter -= Time.deltaTime;
+		if (asteroidSpawnCounter < 0)
+		{
+            Camera cam = Camera.main;
+            float height = 2f * cam.orthographicSize;
+            float width = height * cam.aspect;
+
+            Vector3 spawnPos = new Vector3(UnityEngine.Random.value*width - width/2, UnityEngine.Random.value*height - height/2, 0);
+		    Instantiate(asteroid, spawnPos, Quaternion.identity);
+			Debug.Log("oh no an asteroid");
+			asteroidSpawnCounter = UnityEngine.Random.Range(asteroidSpawnMinTime, asteroidSpawnMaxTime);
+		}
+	}
+}
---
+++ SpaceWave/Assets/Scripts/SpaceStationScript.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceStationScript : MonoBehaviour
+{
+	public float turningSpeed = 5.0f;
+
+	private float targetAngle = 0.0f;
+
+	// Use this for initialization
+	void Start ()
+	{	    
+	}
+	
+	// Update is called once per frame
+	// Do not adjust rigidbodies or colliders etc. in this method!
+	void Update () {
+		// HandleMouseInput();
+		HandleKeyboardInput();
+	}
+
+	void HandleKeyboardInput()
+	{
+		float turn = 0;
+		if (Input.GetKey("a"))
+		{
+			turn += 1;
+		}
+		if (Input.GetKey("d"))
+		{
+			turn += -1;
+		}
+		float angle = transform.eulerAngles.z * Mathf.Deg2Rad;
+		targetAngle = angle + turningSpeed*Time.deltaTime*turn;
+	}
+
+	void HandleMouseInput()
+	{
+		// Get the mouse pointer and turn it into a world vector (ie. an ingame vector)
+		Vector3 mousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		mousePoint.z = 0;
+		// Get target angle to mouse pointer
+		targetAngle = Mathf.Atan2(transform.position.y - mousePoint.y, transform.position.x - mousePoint.x) + Mathf.PI/2f;
+	}
+
+	// FixedUpdate is called once per physics tick
+	// Do ALL physics based calls inside THIS method!!
+	void FixedUpdate()
+	{
+		//float angle = transform.eulerAngles.z * Mathf.Deg2Rad;
+		//float maxSpeed = turningSpeed*Time.deltaTime;
+		//float delta = Mathf.Clamp(targetAngle - angle, -maxSpeed, maxSpeed);
+		// TODO: gracefully elegant sliding towards target
+		transform.rotation = Quaternion.Euler(0, 0, (targetAngle) * Mathf.Rad2Deg);
+
+	}
+}
